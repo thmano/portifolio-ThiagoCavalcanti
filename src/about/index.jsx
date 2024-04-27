864dcb9dc84328d923afbaa0bf5cf336e54fd6f7
thmano <thiago_cavalcanti@live.com>
2024-04-27 09:59:27 -0300

    criando sobre mim

--- src/about/index.jsx
+++ src/about/index.jsx
@@ -1,8 +1,10 @@
+import "./style.css"
 function About() {
 
     return (
       <div className="container_painel">
-        <p>Sobre</p>
+           <h2 className="titleAbout">Aqui estão algumas informações sobre mim e minha história na carreira de desenvolvedor:</h2>
+           <p className="textAbout">Nascido e criado na capital de São Paulo, comecei na area da programção em 2016 cursando na ETEC de guainazes, de lá até aqui nunca tive duvidas de que seria programador, tenho minhas experiencias voltadas para o front-end, mas gosto de me aventurar com back-end e mobile também; já realizei trabalhos como freelancer mas meu maior foco é ser um desenvolvedor com carreira dentro de uma empresa, gosto de todos estilos musicais e alguns dos meus hoobies são: praticar esportes, assistir filmes, ler mangas e estar com os amigos. </p>
       </div>
     )
   }
